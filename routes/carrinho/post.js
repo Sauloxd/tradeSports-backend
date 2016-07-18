7787c4d9623e3af1a42269c6059adb95df3041e5
Danilo <danilodias222@gmail.com>
2016-07-18 15:39:56 -0300

    Fixed logic when adding repeated product in cart

--- routes/carrinho/post.js
+++ routes/carrinho/post.js
@@ -22,13 +22,10 @@ module.exports = function(req, res) {
       console.log(err);
       return res.status(500).json({ success: false, data: err});
     }
+    var queryVerify = client.query("SELECT quantidade FROM Carrinho WHERE cpf_cliente=($1) and id_produto=($2)",[data.CPF, data.idProduto]);
 
     // SQL Query > Insert Data
-    client.query('INSERT INTO Carrinho('            +
-      'cpf_cliente,'                                +
-      'id_produto,'                                 +
-      'quantidade'                                  +
-    ') values($1, $2, $3)', [data.CPF, data.idProduto, data.quantidade]);
+    
 
     // SQL Query > Select Data
     var query = client.query("SELECT * FROM Carrinho ORDER BY cpf_cliente DESC LIMIT 1");
@@ -42,6 +39,25 @@ module.exports = function(req, res) {
     query.on('end', function() {
       done();
       return res.json(results);
+    });
+
+    // Stream results back one row at a time
+    queryVerify.on('row', function(row) {
+      results.push(row);
+    });
+
+    // After all data is returned, close connection and return results
+    queryVerify.on('end', function() {
+      if(results[0] == undefined){
+          client.query('INSERT INTO Carrinho('            +
+                        'cpf_cliente,'                                +
+                        'id_produto,'                                 +
+                        'quantidade'                                  +
+                        ') values($1, $2, $3)', [data.CPF, data.idProduto, data.quantidade]);
+      }else{
+        var quant = parseInt(results[0].quantidade) + parseInt(data.quantidade);
+        client.query("UPDATE Carrinho SET quantidade=($1) WHERE cpf_cliente=($2) and id_produto=($3)", [quant,  data.CPF, data.idProduto]);
+      }
     });
 
 
